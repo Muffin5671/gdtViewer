682fd4858fca78f620a85e06a550f2effc9821c2
Muffin5671 <213143890+Muffin5671@users.noreply.github.com>
2026-06-28 01:23:20 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -21,7 +21,29 @@ function GDTtoJSON(gdt) {
   return json;
 }
 
+function convertBGColorToHWB(tint, brightness) {
+  let h = 216 + tint * 1.8 % 360;
+  let w;
+  let b;
+  // brightness isn't 100% accurate but it works
+  if (brightness < 0) {
+    w = 0;
+    b = 0 - brightness;
+  } else {
+    w = brightness;
+    b = 0;
+  }
+  return `hwb(${h}deg ${w}% ${b}%)`;
+}
+
 let json = GDTtoJSON(level);
+
+let bg = {
+  id: level.split("~")[0].split("_")[0],
+  color: level.split("~")[0].split("_")[1],
+  brightness: level.split("~")[0].split("_")[2]
+};
+canvas.style.backgroundColor = convertBGColorToHWB(bg.color, bg.brightness);
 
 canvas.width = json[json.length - 1].x + 600;
 canvas.height = screen.height;
